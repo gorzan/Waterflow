91e83badfd7c5f6ac4644dffcd7e84275d640a2c
gorzan <gorzan@gmail.com>
2016-06-20 20:03:41 +0200

    Test av api

--- waterflow.py
+++ waterflow.py
@@ -6,8 +6,18 @@ import json
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
-def hello():
-	return "Hello world"
+def waterflow():
+
+	#create response dict
+	output = {}
+	output['response_type'] = 'in_channel'
+	output['text'] = 'Test av vannstrøm'
+
+	json_response = json.dumps(output)
+
+	resp = Response(response=json_response, status=200, mimetype="application/json")
+	
+	return resp
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
