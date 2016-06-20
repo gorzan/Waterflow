552aa9fe8b32060d0163d415f69265b1e3d3d0d8
gorzan <gorzan@gmail.com>
2016-06-20 21:20:11 +0200

    URL til gif

--- waterflow.py
+++ waterflow.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from flask import Flask, jsonify, Response, request
+from bs4 import BeautifulSoup
+import urllib2
 import requests
 import simplejson
 import json
@@ -9,11 +11,37 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def waterflow():
+	
+	#check if loc is defined
+	if 'text' not in request.args:
+		return "Hvilket vassdrag vil du vite vannføringen i?. Bruk '/vannføring [vassdrag]'."
+
+	#get input from slack
+	vassdrag = request.args.get('text').lower()
+
+	#basic url
+	base_url = 'http://www2.nve.no/h/hd/plotreal/Q/'
+	list_url = base_url + 'list.html'
+
+	
+	#Build dict of place / uri pairs
+	soup = BeautifulSoup(urllib2.urlopen(list_url).read(),'html.parser')
+	table = soup.find('table')
+	rows = table.findAll('tr')
+	river = {}
+	for tr in rows:
+		link = tr.find('a', href=True)
+		river[link.text] = link['href'][:-11]
+
+	if vassdrag not in river:
+		return "Ukjent vassdrag."
+
+
 
 	#create response dict
 	output = {}
 	output['response_type'] = 'in_channel'
-	output['text'] = 'Test av vannstrøm'
+	output['text'] = base_url + river[vassdrag] + '.gif'
 
 	json_response = json.dumps(output)
 
